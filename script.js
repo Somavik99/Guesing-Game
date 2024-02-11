3e425008c45aed4a827b9f0bcd60fae112dde6e3
Avik Roy <somavik99@gmail.com>
2024-02-12 00:23:08 +0530

    main

--- script.js
+++ script.js
@@ -3,24 +3,38 @@ const CheckButton = document.getElementById("Check__btn");
 const TextOutput = document.getElementById("Guess__text");
 const TotalTrial = document.getElementById("Total__Trial");
 const CloseEvent = document.getElementById("Close__popup");
+const AgainEvent = document.getElementById("Again__event");
 
 let NumberLimit = 20;
 let RandomNumber = Math.floor(Math.random() * NumberLimit);
+// console.log(RandomNumber);
 let TrialValue = 10;
 let NewTrialValue = TrialValue;
 TotalTrial.textContent = NewTrialValue;
+
+AgainEvent.addEventListener("click", function () {
+  CheckButton.disabled = false;
+  RandomNumber = Math.floor(Math.random() * NumberLimit);
+  GuessingNumber.textContent = "?";
+  GuessingNumber.style.background = "rgb(205, 53, 53)";
+  NewTrialValue = 10
+  TotalTrial.textContent = NewTrialValue;
+  // console.log(RandomNumber);
+});
 
 CheckButton.addEventListener("click", function () {
   const NumberInputValue = document.getElementById("Num__input").value;
   CheckValueEquality(NumberInputValue);
   NewTrialValue === 0
     ? ((CheckButton.disabled = true),
+      (AgainEvent.disabled = true),
       (document.getElementById("Pop__up").style.display = "block"))
     : (CheckButton.disabled = false);
 });
 
 CloseEvent.addEventListener("click", function () {
   document.getElementById("Pop__up").style.display = "none";
+  AgainEvent.disabled = false;
 });
 
 function CheckValueEquality(value) {
