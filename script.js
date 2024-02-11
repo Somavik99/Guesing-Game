e610e05db6cdb38fffbbd13d10b29a6822f1ffa0
Avik Roy <somavik99@gmail.com>
2024-02-12 00:06:41 +0530

    main

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const GuessingNumber = document.getElementById("guess");
 const CheckButton = document.getElementById("Check__btn");
 const TextOutput = document.getElementById("Guess__text");
 const TotalTrial = document.getElementById("Total__Trial");
+const CloseEvent = document.getElementById("Close__popup");
 
 let NumberLimit = 20;
 let RandomNumber = Math.floor(Math.random() * NumberLimit);
@@ -9,34 +10,35 @@ let TrialValue = 10;
 let NewTrialValue = TrialValue;
 TotalTrial.textContent = NewTrialValue;
 
+CheckButton.addEventListener("click", function () {
+  const NumberInputValue = document.getElementById("Num__input").value;
+  CheckValueEquality(NumberInputValue);
+  NewTrialValue === 0
+    ? ((CheckButton.disabled = true),
+      (document.getElementById("Pop__up").style.display = "block"))
+    : (CheckButton.disabled = false);
+});
 
-CheckButton.onclick = function(){
-    const NumberInputValue = document.getElementById("Num__input").value;
-    CheckValueEquality(NumberInputValue);
-} 
+CloseEvent.addEventListener("click", function () {
+  document.getElementById("Pop__up").style.display = "none";
+});
 
 function CheckValueEquality(value) {
-    NewTrialValue -=1;
-    TotalTrial.textContent = NewTrialValue;
-    if (value === RandomNumber.toString() ) {
-      GuessingNumber.textContent = RandomNumber;
-      GuessingNumber.style.background = "green"
-      document.getElementById("Highest_Score").textContent = NewTrialValue;
-      TextOutput.textContent = "Congratulations...";
-    } else if (value > RandomNumber) {
-      GuessingNumber.textContent = "?";
-      TextOutput.textContent = "Too High...!";
-    } else if (value < RandomNumber ) {
-      GuessingNumber.textContent = "?";
-      TextOutput.textContent = "Too Low...!";
-    }else if(value===""){
-        GuessingNumber.textContent = "?";
-      TextOutput.textContent = "Start Guessing...!";
-    }else if(NewTrialValue === 0){
-      TotalTrial.textContent = 0
-        document.getElementById("Pop__up").style.display = "block"
-    }
+  NewTrialValue -= 1;
+  TotalTrial.textContent = NewTrialValue;
+  if (value === RandomNumber.toString()) {
+    GuessingNumber.textContent = RandomNumber;
+    GuessingNumber.style.background = "green";
+    document.getElementById("Highest_Score").textContent = NewTrialValue;
+    TextOutput.textContent = "Congratulations...";
+  } else if (value > RandomNumber) {
+    GuessingNumber.textContent = "?";
+    TextOutput.textContent = "Too High...!";
+  } else if (value < RandomNumber) {
+    GuessingNumber.textContent = "?";
+    TextOutput.textContent = "Too Low...!";
+  } else if (value === "") {
+    GuessingNumber.textContent = "?";
+    TextOutput.textContent = "Start Guessing...!";
   }
-
-
-
+}
